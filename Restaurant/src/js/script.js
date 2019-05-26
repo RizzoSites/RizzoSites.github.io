86cf31fbe2f1abc8c5d74bdd6655d47ad3938ae2
Rizzo <timokhinai1955@gmail.com>
2019-05-26 13:55:54 +0300

    finish commit

--- Restaurant/src/js/script.js
+++ Restaurant/src/js/script.js
@@ -1,51 +1,21 @@
 // hamburger menu
 
 $(document).ready(function() {
-  $('.header-navbtn').click(function() {
-    menu();
-}); 
+  $("#header-navbtn").click(function() {
+    $(this).toggleClass("header-navbtn__active");
+    $(".header-nav").toggleClass("header-nav-open");
 
-
-
-  if($('.header-nav__active')) {
-    $('.header-nav__item').click(function() {
-     $(this).closest('.header-nav').removeClass('header-nav__active');
-     $('body').removeClass('lock-scroll');
+    // this line ▼ prevents content scroll-behind
+    lockScroll();
     });
-  };
+
+   $(".header-nav__item").click(function() {
+    $(".header-nav").removeClass("header-nav-open");
+    $(".header-navbtn").removeClass("header-navbtn__active");
+    $('body').removeClass('lock-scroll');
+});
 });
 
-
-  if($('.header-nav_active')) {
-    $('.header-nav__item').click(function() {
-      menu();
-    });
-  };
-
-
-function menu() {
-  nav();
-  navbtn();
-};
-
-function navbtn() {
-  $('.header-navbtn').toggleClass('header-navbtn__active');
-};
-
-function nav() {
-    $('.header-nav').toggleClass('header-nav__active');
-  };
-
-// lock body (overflow hidden) when mobile menu is opened
-
-      function lockScroll() {
-            if ($('body').hasClass('lock-scroll')) {
-                $('body').removeClass('lock-scroll');
-            }
-            else {
-                $('body').addClass('lock-scroll');
-            }
-      }
 // anchor scroll
 
 $(function(){
