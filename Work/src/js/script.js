e3399d4b56606b214687f6e3d24ee3d40b1ac953
Rizzo <timokhinai1955@gmail.com>
2019-06-05 14:52:49 +0300

    finish commit

--- Work/src/js/script.js
+++ Work/src/js/script.js
@@ -2,12 +2,14 @@
 $(document).ready(function(){
   $('.feedback-slider').slick({
   });
-
-  // открытие модального окна
-  modalOpen();
-  $('.closeModal').click(function() {
-        $('body').removeClass('lock-scroll');   
+  $('.header-btn').click(function() {
+    $('.modalDialog').toggleClass('modal-active');
   });
+  $('.closeModal').click(function() {
+      $('body').removeClass('lock-scroll');   
+      $('.modalDialog').removeClass('modal-active');
+  });
+  // открытие модального окна
   
   width = $(window).width();
     if (width <= 991) {
@@ -51,57 +53,4 @@ function swap() {
     newsIndex = 1;
   }
 }
-
-var modalOpen = function() { // главная функция
-
-  var menuOpen = document.querySelectorAll('.header-btn, .open-btn');
-
-    $(menuOpen).click(function() { /* выбираем класс icon-menu и
-               добавляем метод click с функцией, вызываемой при клике */
-        $('.header-btn').removeClass('header-btn__active');
-        $('.header-btn').toggleClass('header-btn__active');
-
-        $('.modalDialog ').animate({ //выбираем класс menu и метод animate
-
-            right: '0px' /* теперь при клике по иконке, меню, скрытое за
-               левой границей на 285px, изменит свое положение на 0px и станет видимым */
-
-        }, 200); //скорость движения меню в мс
-        
-        $('body').animate({ //выбираем тег body и метод animate
-
-            right: '-1024x' /* чтобы всё содержимое также сдвигалось вправо
-               при открытии меню, установим ему положение 285px */
-
-        }, 700); //скорость движения меню в мс
-    });
-
-    // 
-
-    $('#open-modal').click(function() {
-    $('#open-modal').toggleClass('header-btn__active');
-  });
-
-/* Закрытие меню */
-
-    $('.closeModal').click(function() { //выбираем класс icon-close и метод click
-
-        $('.header-btn').removeClass('header-btn__active');
-
-        $('.modalDialog').animate({ //выбираем класс menu и метод animate
-
-            right: '-1024px' /* при клике на крестик меню вернется назад в свое
-               положение и скроется */
-
-        }, 200); //скорость движения меню в мс
-        
-    $('body').animate({ //выбираем тег body и метод animate
-
-            right: '0px' //а содержимое страницы снова вернется в положение 0px
-
-        }, 200); //скорость движения меню в мс
-    });
-};
-
-
 
