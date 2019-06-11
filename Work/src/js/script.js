78df04428c879b6ab5939c9b17cb0b686e813064
Rizzo <timokhinai1955@gmail.com>
2019-06-12 02:16:31 +0300

    finish commit

--- Work/src/js/script.js
+++ Work/src/js/script.js
@@ -2,6 +2,39 @@
 $(document).ready(function(){
   $('.feedback-slider').slick({
   });
+  // подсвечивание якорей в навигации в зависимости от расположения секций
+  $(window).on("scroll", function() { 
+    var scro= $(window).scrollTop();            
+    var scr=0;    // начало страницы - хедер
+    var scr2=800;   // начало меню "посети курс"
+    var scr3=1500;  // начало меню "о чём этот курс
+    var scr4=2750;  // начало меню "программа курса"
+    var scr5=4180;  // начало меню "цены"
+    var scr6=5100;  // начало меню "спикеры"
+    var scr7=6000;  // начало меню "отзывы"
+/*      положение  скрола относительно пункта хедера    */
+    if ( $(window).scrollTop() >= scr && scro <= scr2 ) { $('#m1').addClass('active');}
+     else $('#m1').removeClass('active');
+/*      положение  скрола относительно пункта "посети курс"   */
+  if ( scro > scr2 && scro <= scr3 ) {  $('#m2').addClass('active');}
+     else $('#m2').removeClass('active');
+/*      положение  скрола относительно пункта "о чём этот курс"   */
+    if ( scro > scr3 && scro <= scr4 ) {    $('#m3').addClass('active');}
+     else $('#m3').removeClass('active');
+/*      положение  скрола относительно пункта "программа курса"  */
+  if ( scro > scr4 && scro <= scr5 ) {    $('#m4').addClass('active');}
+     else $('#m4').removeClass('active');
+/*      положение  скрола относительно пункта "цены"  */
+  if ( scro > scr5 && scro <= scr6 ) {    $('#m5').addClass('active');}
+     else $('#m5').removeClass('active');
+/*      положение  скрола относительно пункта "спикеры"  */
+  if ( scro > scr6 && scro <= scr7 ) {    $('#m6').addClass('active');}
+     else $('#m6').removeClass('active');
+/*      положение  скрола относительно пункта "отзывы"  */
+  if ( scro > scr7 ) {    $('#m7').addClass('active');}
+     else $('#m7').removeClass('active');
+    });
+
   // вызов модального окна по нажатию на кнопку "Отправить заявку"
   $('.header-btn').click(function() {
     $(this).toggleClass('header-btn__active')
@@ -18,16 +51,20 @@ $(document).ready(function(){
     $('.modalDialog').toggleClass('modal-active');
     $('.header-btn').toggleClass('header-btn__active')
   });
-    // вызов модального окна по нажатию на кнопку "Записаться", кнопка "Отправить заявку" уезжает за границы
+    // по нажатии кнопки "Отправить" скрыть первое модальное окно и вызвать второе в секции cost
     $('#sign-button').click(function() {
     $('.sign-in__form').hide();
     $('.cost-modal').show()
   });
+    // по нажатии кнопки "Отправить" скрыть первое модальное окно и вызвать второе в секции header
     $('#modal-button').click(function() {
     $('#openModal').hide();
     $('.modal-thankfulness').show()
   });
+   
+    // код, с помощью которого второе и третье модальное окно исчезает через 5 секунд после появления
   $('.cost-modal').bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function(e) { $(this).remove(); });
+  $('.modal-thankfulness').bind('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function(e) { $(this).remove(); });
   // Удаление анимации и локскрина на десктопе   
   width = $(window).width();
     if (width > 1024) {
@@ -39,6 +76,7 @@ $(document).ready(function(){
         });
     }
 });
+
 
 // лок на экран во время модалки
 
