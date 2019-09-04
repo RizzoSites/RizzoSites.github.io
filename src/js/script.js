4f98a9885c7fabba4efa5152b4fe6dd46984beb6
Rizzo <timokhinai1955@gmail.com>
2019-09-05 00:13:30 +0300

    finish commit

--- src/js/script.js
+++ src/js/script.js
@@ -96,3 +96,25 @@ $(document).ready(function(){
   });
 
 });
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("header-tab__content");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("header-tablinks__link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
