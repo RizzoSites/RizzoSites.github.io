0c6f1b6dedd0487526c3d9bf417a1cb73a22941c
Rizzo <timokhinai1955@gmail.com>
2020-04-23 18:36:37 +0300

    finish commit

---
+++ dist/js/script.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".header-body__button").click(function(){$(".modal").toggleClass("modal-active"),$(".overlay").toggleClass("overlay-active")}),$(".modal-note__close").click(function(){$(".modal").removeClass("modal-active"),$(".overlay").removeClass("overlay-active")})});
