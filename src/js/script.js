0c233dee1d7b7ede2a9de0a359651c20fc123331
Rizzo <timokhinai1955@gmail.com>
2020-08-10 15:41:15 +0300

    s

--- src/js/script.js
+++ src/js/script.js
@@ -1,71 +1,38 @@
-var stone;
 $(document).ready(function() {
 
-$('.header-navbtn').click(function() {
-  $('.header-navbtn').toggleClass('header-navbtn__active')
-  $('.header-nav').toggleClass('header-nav__active');
-});
+ // слайдер на странице home
+  $('.examples-slider').slick({
+    autoplay: true
+  });
+    
+    jQuery('img.svg').each(function(){
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
 
- $(".header-nav__item").click(function() {
-    $(".header-nav").removeClass("header-nav__active");
-    $(".header-navbtn").removeClass("header-navbtn__active");
-});
+            jQuery.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
 
-$('.header-button').click(function() {
-  $('#modal-first').toggleClass('modal-active');
-});
+                // Add replaced image ID to the new SVG
+                if(typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image classes to the new SVG
+                if(typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass+' replaced-svg');
+                }
 
-$('.header-phone__button').click(function() {
-  $('#modal-second').toggleClass('modal-active');
-});
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
 
+                // Replace image with new SVG
+                $img.replaceWith($svg);
 
-$('.modal-close').click(function() {
-  $('#modal-first').removeClass('modal-active');
-  $('#modal-second').removeClass('modal-active');
-});
+            }, 'xml');
 
-$(function(){
-        $("a[href^='#']").click(function(){
-                var _href = $(this).attr("href");
-                $("html, body").animate({scrollTop: $(_href).offset().top+"px"}, 1000);
-                return false;
         });
-});
-
-$("#form-first").submit(function() {
-  stone = $("input[name='stones']:checked").val();
-    $.ajax({
-      type: "POST",
-      url: "mail.php",
-      data: {
-              name : document.getElementById('form-1-name').value,
-              phone : document.getElementById('form-1-phone').value,
-              email : document.getElementById('form-1-email').value,
-              text : document.getElementById('form-1-text').value,
-              stones : stone
-            }
-    }).done(function() {
-      $("#form-first").trigger("reset");
-      $('.modal').removeClass('modal-active');
-    });
-    return false;
-
-  });
-
-$("#form-second").submit(function() {
-    $.ajax({
-      type: "POST",
-      url: "mail-answer.php",
-      data: $(this).serialize()
-    }).done(function() {
-      $("#form-second").trigger("reset");
-      $('.modal').removeClass('modal-active');
-    });
-    return false;
-  });
-
-
 });
 
   
